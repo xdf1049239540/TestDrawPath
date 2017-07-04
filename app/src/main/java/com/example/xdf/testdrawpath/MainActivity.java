fec5344eda73e9b6078981fcd2e7a7b130561078
xdf <1049239540@qq.com>
2017-07-04 11:36:54 +0800

    第二次提交

--- app/src/main/java/com/example/xdf/testdrawpath/MainActivity.java
+++ app/src/main/java/com/example/xdf/testdrawpath/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.xdf.testdrawpath;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("","");
         final MyView2 view_2= (MyView2) findViewById(R.id.view_2);
         view_2.setOnClickListener(new View.OnClickListener() {
             @Override
